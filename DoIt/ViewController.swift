048a1fcc1190da95141a1057360a05f9e2aded7a
Brian Hassett <bhassett.73@gmail.com>
2017-09-27 16:41:29 -0400

    created tableview and ported outlet to view controller. added methods to call for tableview cell count and cell data.

--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -8,17 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    // outlet from storyboard
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // get the data from THIS class
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // how many rows should the table view have?
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
+    
+    // what is in the cells for the table view?
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Hello?"
+        return cell
+    }
+    
+    // Create "tasks" objects
+    
 
 
 }
